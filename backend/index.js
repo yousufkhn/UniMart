87058e6a120cf6b4f2cda1e8f8828a42dd777786
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2025-04-03 09:49:32 +0530

    chore: db connection setup done

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js"; // Import MongoDB connection
+
+dotenv.config();
+connectDB(); // Call DB connection function
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("UniMart Backend is Running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
